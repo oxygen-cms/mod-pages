bb18a90dce0a628ca4d1ba4e9806dfdf76e7e216
Chris Chamberlain <c.chamberlain@iinet.net.au>
2014-11-15 16:07:24 +1100

    Updated default page theme
    Now looks a little bit snazzy.

--- resources/views/pages/view.blade.php
+++ resources/views/pages/view.blade.php
@@ -3,16 +3,51 @@
     <head>
         <meta charset="UTF-8">
         <title>Website</title>
-        <link rel="stylesheet" href="/test.css">
         <style>
             html {
-                background-color: #eef;
+                font-family: sans-serif;
+            }
+
+            body {
+                margin: 0;
+            }
+
+            p {
+                line-height: 1.5;
+            }
+
+            .Header {
+                padding: 2em;
+                background-color: #eee;
+                border-bottom: 1px solid #ccc;
+                text-align: center;
+            }
+
+            .Header-title {
+                font-weight: 200;
+            }
+
+            .Content {
+                padding: 2em;
+                min-height: 100vh;
+            }
+
+            .Footer {
+                background-color: #333;
+                color: #fff;
+                padding: 2em;
             }
         </style>
     </head>
     <body>
-        Theme Goes Here
-        <hr>
-        {{ $content }}
+        <header class="Header">
+            <h1 class="Header-title">Oxygen Example Theme</h1>
+        </header>
+        <div class="Content">
+            {{ $content }}
+        </div>
+        <header class="Footer">
+            <p>Created by Oxygen</p>
+        </header>
     </body>
 </html>
