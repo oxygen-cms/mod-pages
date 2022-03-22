8191443d612b09639c13eb312598ce83531cf256
Chris Chamberlain <chris@chamberlain.id.au>
2022-03-22 20:21:36 +1100

    add default templates if no other templates are set

---
+++ resources/views/pages/content.blade.php
@@ -0,0 +1,24 @@
+<style>
+    html {
+        font-family: sans-serif;
+    }
+
+    body {
+        margin: 0;
+    }
+
+    p {
+        line-height: 1.5;
+    }
+
+    .Inner {
+        max-width: 1000px;
+        margin: 0 auto;
+        padding: 4em;
+    }
+</style>
+
+<div class="Inner">
+    {!! $content !!}
+</div>
+
